476fcfd9b6c05af91e1de199fc332b374cbc038a
Иван Мельчаков <imelchakov@gmail.com>
2023-09-22 14:49:00 +0300

    Update lab7.cpp

--- labs/lab7.cpp
+++ labs/lab7.cpp
@@ -9,39 +9,34 @@ int main() {
     if (x <= -2){
         cout << "Y1 = 0" << "\n";
     } 
-    if (-2 < x)
+    else if (x <= -1)
     {
-        if (x < -1)
-        {
-           cout << "Y1 = -1" << "\n";
-        }
+        cout << "Y1 = -1" << "\n";
     } 
-    if (-1 < x)
+    else if (x < 1)
     {
-        if (x < 1)
-        {
-            cout << "Y1 = 1" << "\n";
-        }
-        
-    } 
-    if (1 <= x)
+        cout << "Y1 = 1" << "\n";
+    }
+    else if (1 <= x)
     {
         if (x < 2)
         {
             cout << "Y1 = -1" << "\n";
         }
-    }
-    if (x >= 2)
+    } 
+    else if (x >= 2)
     {
         cout << "Y1 = 0" << "\n";
     }
-    if ((x <= -2) or (x >= 2))
+    if ((x <= -2) || (x >= 2))
     {
         cout << "Y2 = 0" << "\n";
-    } else if (((-2 < x) and (x <= -1)) or ((1 <= x) and (x < 2)))
+    }
+    if (((-2 < x) && (x <= -1)) || ((1 <= x) && (x < 2)))
     {
         cout << "Y2 = -1" << "\n";
-    } else if ((-1 < x) and (x < 1))
+    }
+    if ((-1 < x) && (x < 1))
     {
         cout << "Y2 = 1" << "\n";
     }
