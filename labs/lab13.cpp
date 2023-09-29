c0ac40c3d3711aa50cf5ceee2d37c4d3c704cc66
Иван Мельчаков <imelchakov@gmail.com>
2023-09-29 14:16:32 +0300

    Update lab13.cpp

--- labs/lab13.cpp
+++ labs/lab13.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <list>
+#include <iterator>
+
 using namespace std;
 
 int FirstElwhile (int x, int i = 2) {
@@ -9,24 +12,26 @@ int FirstElwhile (int x, int i = 2) {
     return i;
 }
 
-int Firstelfor (int x, int q, int i = 2) {
-    for (int i1 = 2; i1 < x; ++i1) {
+int Firstelfor (int x, int i = 2) {
+    for (int i1 = 2; i1 <= x; ++i1) {
         if (x % i1 == 0) {
-            q = i1;
+            return i1;
             exit;
         }
     }
-    return q;
+    return 0;
 }
 
 
 int main() {
     int x;
+    list<int> nums1;
+    list<int> nums2;
     cout << "Введите число: ";
     cin >> x;
     if (x >= 2){
         cout << "1) через while: " << FirstElwhile(x) << endl;
-        cout << "1) через for: " << FirstElwhile(x) << endl;
-    }
+        cout << "1) через for: " << Firstelfor(x) << endl;
+    }   
     return 0;
 }
