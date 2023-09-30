488281c18b6552876c8d376881abd738eafaabc3
Иван Мельчаков <imelchakov@gmail.com>
2023-09-30 03:20:36 +0300

    Update lab13.cpp

--- labs/lab13.cpp
+++ labs/lab13.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <cmath>
-#include <list>
-#include <iterator>
+#include <vector>
 
 using namespace std;
 
@@ -24,14 +22,69 @@ int Firstelfor (int x, int i = 2) {
 
 
 int main() {
-    int x;
-    list<int> nums1;
-    list<int> nums2;
+    int x, i = 2;
+    vector<int> nums1;
+    vector<int> nums2;
     cout << "Введите число: ";
     cin >> x;
     if (x >= 2){
         cout << "1) через while: " << FirstElwhile(x) << endl;
         cout << "1) через for: " << Firstelfor(x) << endl;
+        i = 2;
+        while (i <= x)
+        {
+            if (x % i == 0)
+            {
+                nums1.push_back(i);
+            }
+            ++i;
+        }
+        cout << "2) через while: ";
+        for (int elem : nums1)
+        {
+            cout << elem << " ";
+        } 
+        cout << endl;
+        for (int i = 2; i <= x; ++i) 
+        {
+            if (x % i == 0)
+            {
+                nums2.push_back(i);
+            }
+        }
+        cout << "2) через for: ";
+        for (int elem : nums2)
+        {
+            cout << elem << " ";
+        } 
+        cout << endl;
+        bool answh = true;
+        bool ansfr = true;
+        for (int i = 2; i <= x/2; ++i){
+            if (x % i == 0){
+                ansfr = false;
+                break;
+            }
+        }
+        if (ansfr){
+            cout << "3) через for: простое\n";
+        } else{
+            cout << "3) через for: не простое\n";
+        }
+        i = 2;
+        while (i < x)
+        {
+            if (x % i == 0){
+                answh = false;
+                break;
+            }
+            ++i;
+        }
+        if (answh){
+            cout << "3) через while: простое\n";
+        } else{
+            cout << "3) через while: не простое\n";
+        }
     }   
     return 0;
 }
